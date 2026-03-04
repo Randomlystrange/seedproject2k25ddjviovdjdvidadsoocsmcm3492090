3754d27ac21303caa389505e37cdb299ab8a47a5
Randomlystrange <170492684+Randomlystrange@users.noreply.github.com>
2026-03-04 20:09:41 +0530

    test

--- src/components/CanvasCursor.tsx
+++ src/components/CanvasCursor.tsx
@@ -2,7 +2,7 @@ import useCanvasCursor from '@/hooks/use-canvasCursor';
 
 const CanvasCursor = () => {
   useCanvasCursor();
-  return <canvas className="pointer-events-none fixed inset-0" id="canvas" style={{ zIndex: 99999 }} />;
-};
 
+  return <canvas className='pointer-events-none fixed inset-0' id='canvas' />;
+};
 export default CanvasCursor;
